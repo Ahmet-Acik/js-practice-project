54b2887a932b711e9a5f257c70c56d7103206fb9
drahmetacik <a.acik@icloud.com>
2025-06-07 22:59:55 +0100

    Refactor array methods example to enhance clarity and consistency

--- arrayfunctions.js
+++ arrayfunctions.js
@@ -5,3 +5,9 @@
 const arr = [1, 2, 3];
 const doubled = arr.map(x => x * 2); // [2, 4, 6]
 console.log(doubled);
+
+// 2. Using `filter` to filter elements in an array
+// This code demonstrates the use of the `filter` method to filter elements in an array 
+const filtered = arr.filter(x => x % 2 ===0 ); // [2]
+console.log(filtered);
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,12 @@
+// Array Methods
+
+// 1. Using `map` to transform an array
+// This code demonstrates the use of the `map` method to transform an array by doubling each element.
 const arr = [1, 2, 3];
 const doubled = arr.map(x => x * 2); // [2, 4, 6]
 console.log(doubled);
+
+// 2. Using `filter` to filter elements in an array
+// This code demonstrates the use of the `filter` method to filter elements in an array 
+const filtered = arr.filter(x => x % 2 ===0 ); // [2]
+console.log(filtered);
