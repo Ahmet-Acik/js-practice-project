7c1d8fb7f3d2b0a53855ff3aa7ae2bf1bb97711d
drahmetacik <a.acik@icloud.com>
2025-06-07 22:57:52 +0100

    Add array methods example demonstrating the use of map to double array elements

---
+++ arrayfunctions.js
@@ -0,0 +1,7 @@
+// Array Methods
+
+// 1. Using `map` to transform an array
+// This code demonstrates the use of the `map` method to transform an array by doubling each element.
+const arr = [1, 2, 3];
+const doubled = arr.map(x => x * 2); // [2, 4, 6]
+console.log(doubled);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,3 @@
-var isStudent = true;
-console.log(isStudent);
-var x = null;
-console.log(x);
-var y = undefined;
-console.log(y);
-var z = Symbol('example');
-console.log(z);
+const arr = [1, 2, 3];
+const doubled = arr.map(x => x * 2); // [2, 4, 6]
+console.log(doubled);
